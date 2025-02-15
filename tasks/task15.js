61a0507500dd3c56bdeec687a0f6b779a6face9c
Levonze <raupenzelevonze@gmail.com>
2025-02-15 11:33:34 +0500

    Added task 15

---
+++ tasks/task15.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('task3-form');
+    if (form) {
+        form.addEventListener('submit', function (event) {
+            event.preventDefault(); // Предотвращаем отправку формы
+
+            // Правильные ответы
+            const correctAnswers = {
+                1: "5",
+                2: "6",
+                3: "2",
+                4: "1",
+                5: "3",
+                6: "2",
+                7: "окислитель",
+                8: "1",
+                9: "восстановитель"
+            };
+
+            // Получаем все поля ввода и выбора
+            const inputs = document.querySelectorAll('#task3-form input, #task3-form select');
+            let isCorrect = true;
+
+            inputs.forEach(input => {
+                const userAnswer = input.value.trim(); // Получаем ответ пользователя
+                const correctAnswer = correctAnswers[input.name]; // Получаем правильный ответ
+
+                if (userAnswer !== correctAnswer) {
+                    isCorrect = false; // Если хотя бы один ответ неверный, устанавливаем флаг в false
+                }
+            });
+
+            // Выводим результат
+            const resultDiv = document.getElementById('result');
+            if (isCorrect) {
+                resultDiv.innerHTML = `<p><img src="/assets/right.png" height="25"> Все верно!<br><a href="task16.html">Задание 16</a></p>`;
+            } else {
+                resultDiv.innerHTML = `<p><img src="/assets/wrong.png" height="25"> Есть ошибки в ответе!</p>`;
+            }
+        });
+    }
+});
