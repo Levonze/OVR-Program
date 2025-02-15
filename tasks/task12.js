686d5786bae4d0939fa481be3ec2c4847c17d4cc
Levonze <raupenzelevonze@gmail.com>
2025-02-15 10:20:28 +0500

    Update and Patch
    Added task 12
    Fixed grammar error

---
+++ tasks/task12.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('task3-form');
+    if (form) {
+        form.addEventListener('submit', function (event) {
+            event.preventDefault(); // Предотвращаем отправку формы
+
+            // Правильные ответы
+            const correctAnswers = {
+                1: "1",
+                2: "2",
+                3: "1",
+                4: "1",
+                5: "1",
+                6: "1",
+                7: "окислитель",
+                8: "1",
+                9: "восстановитель"
+            };
+
+            // Получаем все поля ввода и выбора
+            const inputs = document.querySelectorAll('#task3-form input, #task3-form select');
+            let isCorrect = true;
+
+            inputs.forEach(input => {
+                const userAnswer = input.value.trim(); // Получаем ответ пользователя
+                const correctAnswer = correctAnswers[input.name]; // Получаем правильный ответ
+
+                if (userAnswer !== correctAnswer) {
+                    isCorrect = false; // Если хотя бы один ответ неверный, устанавливаем флаг в false
+                }
+            });
+
+            // Выводим результат
+            const resultDiv = document.getElementById('result');
+            if (isCorrect) {
+                resultDiv.innerHTML = `<p><img src="right.png" height="25"> Все верно!<br><a href="task13.html">Задание 13</a></p>`;
+            } else {
+                resultDiv.innerHTML = `<p><img src="wrong.png" height="25"> Есть ошибки в ответе!</p>`;
+            }
+        });
+    }
+});
